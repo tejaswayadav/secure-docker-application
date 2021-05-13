9c3e6529abfaf04d99a03955a8f1734aebcaef03
Tejaswa Yadav <tejaswayadav@gmail.com>
2021-05-13 08:41:14 +0530

    Added Controllers, Services and Models

--- src/main/java/com/tejaswa/dockerapp/controller/SecureController.java
+++ src/main/java/com/tejaswa/dockerapp/controller/SecureController.java
@@ -1,18 +1,57 @@
 package com.tejaswa.dockerapp.controller;
 
+import com.tejaswa.dockerapp.model.Student;
+import com.tejaswa.dockerapp.model.Teacher;
 import com.tejaswa.dockerapp.service.SecureService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "/v1/secure")
 public class SecureController {
 
-    private SecureService secureService;
+    private final SecureService secureService;
 
     @Autowired
     public SecureController(SecureService secureService) {
         this.secureService = secureService;
     }
+
+    @GetMapping("/student/{studentId}")
+    @PreAuthorize("hasAuthority('student:read')")
+    public Student getStudentById(@PathVariable Long studentId) {
+        return secureService.getStudentById(studentId);
+    }
+
+    @PostMapping("/student/create")
+    @PreAuthorize("hasAuthority('student:write')")
+    public Student createStudent(@RequestBody Student student) {
+        return secureService.createStudent(student);
+    }
+
+    @DeleteMapping("/student/{studentId}")
+    @PreAuthorize("hasAuthority('student:write')")
+    public Boolean deleteStudent(@PathVariable Long studentId) {
+        return secureService.deleteStudent(studentId);
+    }
+
+    @GetMapping("/teacher/{teacherId}")
+    @PreAuthorize("hasAuthority('teacher:read')")
+    public Teacher getTeacherById(@PathVariable Long teacherId) {
+        return secureService.getTeacherById(teacherId);
+    }
+
+    @PostMapping("/teacher/create")
+    @PreAuthorize("hasAuthority('teacher:write')")
+    public Teacher createTeacher(@RequestBody Teacher teacher) {
+        return secureService.createTeacher(teacher);
+    }
+
+    @DeleteMapping("/teacher/{teacherId}")
+    @PreAuthorize("hasAuthority('teacher:write')")
+    public Boolean deleteTeacher(@PathVariable Long teacherId) {
+        return secureService.deleteTeacher(teacherId);
+    }
+
 }
--- src/main/java/com/tejaswa/dockerapp/service/SecureService.java
+++ src/main/java/com/tejaswa/dockerapp/service/SecureService.java
@@ -1,7 +1,61 @@
 package com.tejaswa.dockerapp.service;
 
+import com.tejaswa.dockerapp.model.Student;
+import com.tejaswa.dockerapp.model.Teacher;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+import static com.tejaswa.dockerapp.model.Repository.STUDENT_LIST;
+import static com.tejaswa.dockerapp.model.Repository.TEACHER_LIST;
 
 @Service
 public class SecureService {
+    public Student getStudentById(Long studentId) {
+        Optional<Student> optionalStudent = STUDENT_LIST.stream()
+                .filter(student -> student.getId().equals(studentId))
+                .findFirst();
+
+        return optionalStudent.orElse(null);
+    }
+
+    public Teacher getTeacherById(Long teacherId) {
+        Optional<Teacher> optionalTeacher = TEACHER_LIST.stream()
+                .filter(teacher -> teacher.getId().equals(teacherId))
+                .findFirst();
+
+        return optionalTeacher.orElse(null);
+    }
+
+    public Student createStudent(Student student) {
+        STUDENT_LIST.add(student);
+        return student;
+    }
+
+    public Teacher createTeacher(Teacher teacher) {
+        TEACHER_LIST.add(teacher);
+        return teacher;
+    }
+
+    public Boolean deleteStudent(Long studentId) {
+        Optional<Student> optionalStudent = STUDENT_LIST.stream()
+                .filter(student -> student.getId().equals(studentId))
+                .findFirst();
+        if (optionalStudent.isPresent()) {
+            STUDENT_LIST.remove(optionalStudent.get());
+            return true;
+        }
+        return false;
+    }
+
+    public Boolean deleteTeacher(Long teacherId) {
+        Optional<Teacher> optionalTeacher = TEACHER_LIST.stream()
+                .filter(teacher -> teacher.getId().equals(teacherId))
+                .findFirst();
+        if (optionalTeacher.isPresent()) {
+            TEACHER_LIST.remove(optionalTeacher.get());
+            return true;
+        }
+        return false;
+    }
 }
